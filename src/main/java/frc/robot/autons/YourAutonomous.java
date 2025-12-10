9268d6c481b1ec8471dcf2e02eeab5327f75737e
cs2500 <claradsue@gmail.com>
2025-12-10 17:50:55 -0500

    Update YourAutonomous.java
    everything up to driveArc

--- src/main/java/frc/robot/autons/YourAutonomous.java
+++ src/main/java/frc/robot/autons/YourAutonomous.java
@@ -1,6 +1,8 @@
 package frc.robot.autons;
 
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import frc.robot.commands.DriveDistance;
+import frc.robot.commands.TurnDegrees;
 import frc.robot.subsystems.Drivetrain;
 
 public class YourAutonomous extends SequentialCommandGroup {
@@ -9,6 +11,16 @@ public class YourAutonomous extends SequentialCommandGroup {
             /* ROOKIES, INSERT DRIVE COMMANDS HERE TO CREATE YOUR AUTON! 
              * EX: new DriveTime(parameter, parameter, parameter)
             */
+            new TurnDegrees(0.5, 90, drivetrain),
+            new DriveDistance(0.5, 4.5 ,drivetrain),
+            new TurnDegrees(-0.5, 90, drivetrain),
+            new DriveDistance(0.5, 7 , drivetrain),
+            new TurnDegrees(-0.5, 90, drivetrain), 
+            new DriveDistance(0.5, 7 , drivetrain),
+            new TurnDegrees(0.5, 90, drivetrain),
+            new DriveDistance(0.5, 4.5 ,drivetrain)
+            //turn
+            //driveArc
         );
     }
 }
